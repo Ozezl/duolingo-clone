3ba49208a02dfc67fd82f87b2c16201f2393b1cc
Aziz Ibragimov <ozezlus@mail.ru>
2020-08-31 01:34:19 +0200

    Updates

---
+++ precache-manifest.c859e0702a1130c2d3878cc996e27f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005bea4711e85f880301b0da9fd5e6b7",
+    "url": "/duolingo-clone/index.html"
+  },
+  {
+    "revision": "aeb844f808bfd8ed311c",
+    "url": "/duolingo-clone/static/css/main.b37e9012.chunk.css"
+  },
+  {
+    "revision": "41bec2552f3ac755f0e0",
+    "url": "/duolingo-clone/static/js/2.4d80e5b6.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/duolingo-clone/static/js/2.4d80e5b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb844f808bfd8ed311c",
+    "url": "/duolingo-clone/static/js/main.352277f9.chunk.js"
+  },
+  {
+    "revision": "e1f038a736cc878f7d9d",
+    "url": "/duolingo-clone/static/js/runtime-main.65251a3a.js"
+  }
+]);
